c1d4a22d19c42ba2623614aa4d9e97c6c3367acd
calrawdatain <calrawdatain@gmail.com>
2026-05-19 18:54:51 +0300

    lash fix

--- dashboard/classes/core.php
+++ dashboard/classes/core.php
@@ -1,18 +1,13 @@
 <?php
 
-
-
 class Core {
 
-  public function ip () {
-    return $_SERVER['REMOTE_ADDR'];
-  }
+    public function ip () {
+        return $_SERVER['REMOTE_ADDR'];
+    }
 
-  public function refresh () {
-    echo '<meta http-equiv="refresh" content="0"/>';
-  }
+    public function refresh () {
+        return '<meta http-equiv="refresh" content="0"/>';
+    }
 
 }
-
-?>
-
